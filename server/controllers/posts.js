b5727ba9fb2c476c036878d37bdd0f973eb114c2
Shahim <muhdshahim786@gmail.com>
2021-05-22 13:42:01 +0530

    completed a router for get and post using try catch. if any req/res from client to server it will work well. so next step is to crete a get and post req form client side using material UI. (we forget to install metiral ui at the first of the project.)

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,10 +1,28 @@
 //this is used to avoid complexity of router folder. becuse all functionaltiy
 //run in the same folder it will make more complex. so we can control function in this file
 
-export const getPosts = (req, res) => {
+import PostMessage from "../models/postMessage";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
   res.send("THIS WORKS");
 };
 
-export const createPost = (req, res) => {
+export const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
   res.send("Post Creation");
 };
