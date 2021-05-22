d5318d7f5fe3940095724afd66ced98402f38963
Shahim <muhdshahim786@gmail.com>
2021-05-22 12:54:31 +0530

    Setup index.js file in server dir, connected mongoose with MongoDB Atlas. and also used some middleware for bodyparser and cors

--- server/index.js
+++ server/index.js
@@ -2,3 +2,21 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true })); // maximum file size uploaded in this sever is 30mb
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://dbUser:dbUser@cluster0.cwqzw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
