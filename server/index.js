1015db75d0b7784e76526ad11aa34c19c144f588
Shahim <muhdshahim786@gmail.com>
2021-05-22 13:08:22 +0530

    created router folder in server dir, changed start script in package.json. because berofe changing that it shows some error while running the server.

--- server/index.js
+++ server/index.js
@@ -3,14 +3,18 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts";
+
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true })); // maximum file size uploaded in this sever is 30mb
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 const CONNECTION_URL =
-  "mongodb+srv://dbUser:dbUser@cluster0.cwqzw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://dbUser:dbUser@cluster0.cwqzw.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
